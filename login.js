fa4c2efdf9025d7b5a7e38fd81e374f90150ecab
Md Faizan Raza <115460409+Fiza32@users.noreply.github.com>
2022-11-01 21:51:12 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
 let user_name = "abc123@gmail.com";
 
 let user_pass = "abc1234";
+
+let data_user = "abc123@gmail.con";
+let data_pass = "abc1234";
+
+if (user_name === data_user && user_pass === data_pass) {
+  console.log("Congratulations! You have successfully Logged In")
+} else {
+  console.log("Wrong Credentials, Please check your Username and Password")
+}
+
